a289bf3a8147253dfb652463ed624ac0c70beede
sahassan012 <sahassan012@gmail.com>
2023-12-12 16:39:27 -0600

    add mock data

--- livedatalab-ui/src/data/mockData.js
+++ livedatalab-ui/src/data/mockData.js
@@ -5,17 +5,36 @@ export const mockProjects = [
 		description: "Description for Project Alpha",
 	},
 	{ id: 2, name: "Project Beta", description: "Description for Project Beta" },
-	// ...other projects
 ];
 
 export const mockCourses = [
 	{ id: 1, name: "Course Alpha", description: "Description for Course Alpha" },
 	{ id: 2, name: "Course Beta", description: "Description for Course Beta" },
-	// ...other courses
 ];
 
 export const mockLeaderboard = [
 	{ id: 1, user: "User1", score: 100 },
 	{ id: 2, user: "User2", score: 95 },
-	// ...other leaderboard entries
+];
+
+export const mockLinkedAccounts = [
+	{ id: 1, user: "Account1", github: "johnab@github.com" },
+	{ id: 2, user: "Accont2", github: "sabob@github.com" },
+];
+
+export const mockSubmissions = [
+	{
+		id: 1,
+		user: "Account1",
+		githubRepo: "github.com/johnab/MP3.2_git",
+		time: "9/22/2023, 7:07:10 AM",
+		submissionNumber: 1,
+	},
+	{
+		id: 2,
+		user: "Account2",
+		githubRepo: "github.com/johnab/MP3.2_git",
+		time: "9/22/2023, 7:09:10 AM",
+		submissionNumber: 3,
+	},
 ];
